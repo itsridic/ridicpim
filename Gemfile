c5607777e9633209c6291e1e4aa4f36269a75434
Nate Dalo <nate.dalo@yahoo.com>
2016-08-16 12:09:51 -0500

    add quickbooks-ruby gem and initializer

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'apartment', '~> 1.2'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'font-awesome-rails', '~> 4.6', '>= 4.6.3.1'
 gem 'country_select', '~> 2.5', '>= 2.5.2'
+gem 'quickbooks-ruby', '~> 0.4.4'
 
 group :development, :test do
   gem 'guard', '~> 2.14'
---
+++ config/initializers/quickbooks.rb
@@ -0,0 +1,13 @@
+QB_KEY = ENV["QB_KEY"]
+QB_SECRET = ENV["QB_SECRET"]
+
+$qb_oauth_consumer = OAuth::Consumer.new(QB_KEY, QB_SECRET), {
+  :site                 => "https://oauth.intuit.com",
+  :request_token_path   => "/oauth/v1/get_request_token",
+  :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
+  :access_token_path    => "/oauth/v1/get_access_token"
+})
+
+Quicbooks.sandbox_mode = true
+Quickbooks.log = true
+Quickbooks.logger = Railer.logger
