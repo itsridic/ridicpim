5822c6f4e3e35f107a6e26323b9eba7f3014e109
Nate Dalo <nate.dalo@yahoo.com>
2016-09-07 17:22:40 -0500

    precompile assets; move tether before bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require tether
+//= require bootstrap-sprockets
 //= require mdb/mdb
 //= require datetimepicker/jquery.datetimepicker
 //= require cocoon
