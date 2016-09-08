bd9c03989bba9abeab9d83ab7157a540f3ef42b8
Nate Dalo <nate.dalo@yahoo.com>
2016-09-08 10:15:22 -0500

    fix issue where files were creating with tabs instead of spacing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   #skip_before_action :set_quickbooks_base_config
   
-	def index
-	end
+  def index
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,24 +1,24 @@
 class Account < ApplicationRecord
-	RESTRICTED_SUBDOMAINS = %w(www)
+  RESTRICTED_SUBDOMAINS = %w(www)
 
-	belongs_to :owner, class_name: 'User'
-	validates :owner, presence: true
+  belongs_to :owner, class_name: 'User'
+  validates :owner, presence: true
 
-	validates :subdomain, presence: true,
-												uniqueness: { case_sensitive: false },
-												format: { with: /\A[\w\-]+\Z/i, message: 'contains invalid characters' },
-												exclusion: { in: RESTRICTED_SUBDOMAINS, message: 'restricted' }
+  validates :subdomain, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        format: { with: /\A[\w\-]+\Z/i, message: 'contains invalid characters' },
+                        exclusion: { in: RESTRICTED_SUBDOMAINS, message: 'restricted' }
 
-	accepts_nested_attributes_for :owner
-	before_validation :downcase_subdomain
-	has_settings :sales_receipt_default_customer, :sales_receipt_deposit_to_account, :classify_shipping,
-							 :classify_sale_tax, :classify_promotion_shipping, :classify_shipping_sales_tax,
-							 :classify_fba_gift_wrap, :classify_balance_adjustment, :classify_gift_wrap_tax, :classify_unknown,
-							 :sales_receipt_income_account, :expense_bank_account, :expense_customer
+  accepts_nested_attributes_for :owner
+  before_validation :downcase_subdomain
+  has_settings :sales_receipt_default_customer, :sales_receipt_deposit_to_account, :classify_shipping,
+               :classify_sale_tax, :classify_promotion_shipping, :classify_shipping_sales_tax,
+               :classify_fba_gift_wrap, :classify_balance_adjustment, :classify_gift_wrap_tax, :classify_unknown,
+               :sales_receipt_income_account, :expense_bank_account, :expense_customer
 
-	private
+  private
 
-	def downcase_subdomain
-		self.subdomain = subdomain.try(:downcase)
-	end												
+  def downcase_subdomain
+    self.subdomain = subdomain.try(:downcase)
+  end                       
 end
