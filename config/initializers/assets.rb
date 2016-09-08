81bb64ad87c2b656b0a819a1c8d898e211854ac6
Nate Dalo <nate.dalo@yahoo.com>
2016-09-08 14:11:08 -0500

    add assets to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( order-user-date.js.coffee )
+Rails.application.config.assets.precompile += %w( rails.validations.js rails.validations.simple_form.js sales_receipts.coffee products.coffee expense_receipts.coffee )
