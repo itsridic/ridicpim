d2dd1b6dbcbdca1e345c93f2407125ffba3ce94a
Nate Dalo <nate.dalo@yahoo.com>
2016-09-14 14:06:46 -0500

    Task Clash #137: Change No. and Reference No. to be the date range

--- app/workers/sales_receipt_worker.rb
+++ app/workers/sales_receipt_worker.rb
@@ -5,13 +5,16 @@ class SalesReceiptWorker
     puts "Creating Sales Receipt in QBO..."
     current_account = Account.find(current_account_id)
     receipt = SalesReceipt.find(receipt_id)
+    amazon_statement = AmazonStatement.find(amazon_statement_id)
 
     qbo_rails = QboRails.new(QboConfig.last, :sales_receipt)
     qbo_receipt = qbo_rails.base.qr_model(:sales_receipt)
     qbo_receipt.customer_id = current_account.settings(:sales_receipt_default_customer).val
     qbo_receipt.txn_date = Date.parse(receipt.user_date.to_s)
     qbo_receipt.deposit_to_account_id = current_account.settings(:sales_receipt_deposit_to_account).val
-    qbo_receipt.auto_doc_number!    
+    date_time = DateTime.parse(amazon_statement.period.split(" - ")[0]).strftime("%m%d%Y") + "_" + DateTime.parse(amazon_statement.period.split(" - ")[1]).strftime("%m%d%Y")
+    qbo_receipt.doc_number = date_time
+    qbo_receipt.payment_ref_number = date_time
     qbo_rails = QboRails.new(QboConfig.last, :sales_receipt)
     receipt.sales.each do |sale|
       line_item = qbo_rails.base.qr_model(:line)
