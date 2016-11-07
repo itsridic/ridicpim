5a6260ddbdd49f57a34b70bc69aa9eaf90c5127b
Nate Dalo <nate.dalo@yahoo.com>
2016-11-07 11:50:37 -0600

    when changing location on order; update inventory_movement to reflect new location

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   belongs_to :location
   has_many :order_items, dependent: :destroy
   accepts_nested_attributes_for :order_items, reject_if: :all_blank, allow_destroy: true
-  after_save :set_user_date
+  after_save :set_user_date, :update_inventory_movement
 
   validates :name, presence: true
   validates :contact, presence: true
@@ -14,6 +14,13 @@ class Order < ApplicationRecord
   def set_user_date
     if user_date.blank?
       self.update_column(:user_date, self.created_at)
+    end
+  end
+
+  def update_inventory_movement
+    loc = self.location
+    self.order_items.each do |order_item|
+      InventoryMovement.find_by(movement_type: "ORDER", reference_id: order_item.id).update(location: loc)
     end
   end
 end
