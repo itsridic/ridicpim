bea2f249126215ec0ea27ede85c8f9632b7ad2c7
Nate Dalo <nate.dalo@yahoo.com>
2016-11-22 13:22:59 -0600

    update mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,11 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method :smtp
+  host = 'retailmerge.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
