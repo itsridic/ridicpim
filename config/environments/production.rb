7eebe7ed4f97126e4eb1332762c98c2fcd53e1f3
Nate Dalo <nate.dalo@yahoo.com>
2016-11-22 12:37:00 -0600

    add sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,4 +20,13 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
   config.active_record.dump_schema_after_migration = false
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
