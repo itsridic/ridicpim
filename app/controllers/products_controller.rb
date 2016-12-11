0965244667219e8491e383d1b065dc3a80db9906
Nate Dalo <nate.dalo@yahoo.com>
2016-12-11 14:26:41 -0600

    refactor products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,41 +7,26 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-
     respond_to do |format|
       if @product.save
         create_update_product_in_qbo(@product) if QboConfig.exists?
-        format.html { redirect_to @product, flash: { success: 'Product was successfully created.' } }
         format.js {}
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def index
-    @products = Product.all.order("name")
+    @products = Product.all.order('name')
     @product = Product.new
   end
 
-  def show
-  end
-
-  def edit
-  end
+  def show() end
 
   def update
     respond_to do |format|
       if @product.update(product_params)
         create_update_product_in_qbo(@product) if QboConfig.exists?
-        format.html { redirect_to products_path, flash: { success: 'Product was successfully updated.' } }
         format.js {}
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -49,29 +34,15 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.js {}
-      format.json { head :no_content }
     end
   end
 
   def fetch
-    oauth_client = OAuth::AccessToken.new($qb_oauth_consumer, QboConfig.first.token, QboConfig.first.secret)
-    product_service = Quickbooks::Service::Item.new(:access_token => oauth_client, :company_id => QboConfig.realm_id)
+    product_service = create_product_service
     query = "SELECT * FROM Item WHERE active = true AND type = 'NonInventory'"
     product_service.query_in_batches(query, per_page: 1000) do |batch|
-      batch.each do |product|
-        product_sku   = product.sku
-        product_name  = product.name || product.description
-        product_price = product.unit_price || 0
-        product_sku = product_name if product_sku.blank?
-        if Product.where(amazon_sku: product_sku).count == 0
-          Product.create!(name: product_name, amazon_sku: product_sku, price: product_price, qbo_id: product.id)
-        else
-          pr = Product.find_by(amazon_sku: product_sku)
-          pr.update(qbo_id: product.id) if pr
-        end
-      end
+      create_products_from_batch(batch)
     end
     redirect_to products_path
   end
@@ -79,7 +50,8 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :amazon_sku, :price, :qbo_id, :bundle_quantity, :bundle_product_id)
+    params.require(:product).permit(:name, :amazon_sku, :price, :qbo_id,
+                                    :bundle_quantity, :bundle_product_id)
   end
 
   def set_product
@@ -89,12 +61,63 @@ class ProductsController < ApplicationController
   def create_update_product_in_qbo(product)
     qbo_rails = QboRails.new(QboConfig.last, :item)
     item = qbo_rails.base.qr_model(:item)
-    item.income_account_id = current_account.settings(:sales_receipt_income_account).val
-    item.type = "NonInventory"
+    item.income_account_id =
+      current_account.settings(:sales_receipt_income_account).val
+    item.type = 'NonInventory'
     item.name = product.amazon_sku
     item.description = product.name
     item.unit_price = product.price
     item.sku = product.amazon_sku
     qbo_rails.create_or_update(product, item)
+  end
+
+  def set_oauth_client
+    OAuth::AccessToken.new(
+      $qb_oauth_consumer, QboConfig.first.token,
+      QboConfig.first.secret
+    )
+  end
+
+  def create_product_service
+    oauth_client = set_oauth_client
+    Quickbooks::Service::Item.new(
+      access_token:  oauth_client,
+      company_id: QboConfig.realm_id
+    )
+  end
+
+  def create_product(product_name, product_sku, product_price, id)
+    Product.create!(name: product_name,
+                    amazon_sku: product_sku,
+                    price: product_price,
+                    qbo_id: id)
+  end
+
+  def update_product(product_sku, product_id)
+    pr = Product.find_by_amazon_sku(product_sku)
+    pr.update(qbo_id: product_id) if pr
+  end
+
+  def create_products_from_batch(batch)
+    batch.each do |product|
+      details = product_details(product)
+      if Product.find_by_amazon_sku(details[:product_sku]).count.zero?
+        create_product(details[:product_name], details[:product_sku],
+                       details[:product_price], product.id)
+      else
+        update_product(details[:product_sku], product.id)
+      end
+    end
+  end
+
+  def product_details(product)
+    details = {}
+    details[:product_sku] = product.name
+    details[:product_name] = product.name || product.description
+    details[:product_price] = product.unit_price || 0
+    if details[:product_sku].blank?
+      details[:product_sku] = details[:product_name]
+    end
+    details
   end
 end
