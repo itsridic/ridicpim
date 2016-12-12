ac84e2332fdc992d49c2a050eed0800c16981122
Nate Dalo <nate.dalo@yahoo.com>
2016-12-12 13:58:48 -0600

    refactor adjustments_controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,25 +1,24 @@
 class AccountsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
-  skip_before_action :check_active_status, only: [:inactive, :new, :create, :reactivate]
-  before_action :set_plan, only: [:create, :inactive, :reactivate, :new, :show, :edit]
+  skip_before_action :check_active_status,
+                     only: [:inactive, :new, :create, :reactivate]
+  before_action :set_plan,
+                only: [:create, :inactive, :reactivate, :new, :show, :edit]
+  before_action :set_account,
+                only: [:show, :edit, :update, :inactive, :reactivate]
 
   def new
     @account = Account.new
     @account.build_owner
   end
 
-  def show
-    @account = current_account
-  end
+  def show() end
 
-  def edit
-    @account = current_account
-  end
+  def edit() end
 
   def update
-    @account = current_account
     if @account.update_account(@account, params[:stripeToken])
-      redirect_to subdomain_root_path, notice: "Your card has been updated!"
+      redirect_to subdomain_root_path, notice: 'Your card has been updated!'
     else
       render :edit
     end
@@ -27,21 +26,20 @@ class AccountsController < ApplicationController
 
   def create
     @account = Account.new(account_params)
-    if @account.save_with_payment(@plan, params[:stripeToken], params[:stripeEmail])
-      redirect_to new_user_session_url(subdomain: @account.subdomain), notice: "Your account was created successfully"
+    if @account.save_with_payment(@plan, params[:stripeToken],
+                                  params[:stripeEmail])
+      redirect_to new_user_session_url(subdomain: @account.subdomain),
+                  notice: 'Your account was created successfully'
     else
       render :new
     end
   end
 
-  def inactive
-    @account = current_account
-  end
+  def inactive() end
 
   def reactivate
-    @account = current_account
     if @account.reactivate_account(@plan, params[:stripeToken])
-      redirect_to subdomain_root_path, notice: "Thank you for your payment!"
+      redirect_to subdomain_root_path, notice: 'Thank you for your payment!'
     else
       render :inactive
     end
@@ -53,7 +51,13 @@ class AccountsController < ApplicationController
     @plan = Plan.first
   end
 
+  def set_account
+    @account = current_account
+  end
+
   def account_params
-    params.require(:account).permit(:subdomain, :accept_terms, owner_attributes: [:name, :email, :password, :password_confirmation])
+    params.require(:account)
+          .permit(:subdomain, :accept_terms, owner_attributes:
+            [:name, :email, :password, :password_confirmation])
   end
 end
